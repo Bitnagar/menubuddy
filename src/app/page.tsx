7c1c1d8c8639ba550844b4bb69989184f3c43a59
Bitnagar <shivambhatnagar30@gmail.com>
2024-01-06 18:09:05 +0530

    init commit + basic prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,157 @@
-import Image from 'next/image'
+"use client";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useState } from "react";
 
 export default function Home() {
+  const [gender, setGender] = useState("");
+  const [diet, setDiet] = useState("");
+  const [spicy, setSpicy] = useState("");
+  const [allergies, setAllergies] = useState("");
+  const [imageFile, setImage] = useState<any>();
+
+  const genAI = new GoogleGenerativeAI(
+    process.env.NEXT_PUBLIC_API_KEY as string
+  );
+
+  async function getImageBase64(image: File) {
+    const fr = new FileReader();
+    fr.readAsDataURL(image);
+    const promise = new Promise((resolve) => {
+      fr.addEventListener("load", function (evt) {
+        resolve({ data: evt.target?.result, type: image.type });
+      });
+    });
+    return promise;
+  }
+
+  async function fileToGenerativePart() {
+    const { data, type } = (await getImageBase64(imageFile)) as {
+      data: string;
+      type: string;
+    };
+    const srcBase64 = data.slice(data.indexOf(",") + 1);
+    return {
+      inlineData: {
+        data: srcBase64,
+        mimeType: type,
+      },
+    };
+  }
+
+  async function run() {
+    // For text-and-image input (multimodal), use the gemini-pro-vision model
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro-vision",
+      generationConfig: {
+        temperature: 1,
+      },
+    });
+
+    const prompt = `
+      I'm a ${gender}. I eat only ${diet} diet. I prefer ${spicy} spicy food. I am allergic to ${allergies}. Recommend some ${diet} dishes from the menu image provided. Remember my preferences. Do not recommend dish that may cause allergy.
+    `;
+    const imageParts = [await fileToGenerativePart()];
+    const result = await model.generateContent([prompt, ...imageParts]);
+    const response = result.response;
+    const text = response.text();
+    console.log(text);
+  }
+
+  function handleSubmit() {
+    run();
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex flex-col w-[50%] p-4 gap-5">
+      <video id="video"></video>
+      <input
+        type="text"
+        name="gender"
+        placeholder="gender"
+        onChange={(e) => {
+          setGender(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        name="diet"
+        placeholder="Veg Or Non Veg"
+        onChange={(e) => {
+          setDiet(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        name="spicy"
+        placeholder="spiciness: Low or Extra"
+        onChange={(e) => {
+          setSpicy(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        name="allergies"
+        placeholder="do you have any allergies?"
+        onChange={(e) => {
+          setAllergies(e.target.value);
+        }}
+      />
+      <input
+        type="file"
+        name="photo"
+        accept=".jpg .png .jpeg"
+        onChange={(e) => {
+          const files = e.target.files;
+          if (files && files.length > 0) {
+            const file = files[0];
+            setImage(file);
+          }
+        }}
+      />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <button
+        onClick={() => {
+          navigator.mediaDevices
+            .getUserMedia({
+              audio: false,
+              video: { width: 1280, height: 720 },
+            })
+            .then((stream) => {
+              const video = document.querySelector("video");
+              if (video) {
+                video.srcObject = stream;
+                video.onloadedmetadata = () => {
+                  video.play();
+                };
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+      >
+        Camera
+      </button>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <button
+        onClick={() => {
+          const video = document.querySelector("video");
+          if (video) {
+            video.pause();
+          }
+        }}
+      >
+        pause
+      </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        Submit
+      </button>
     </main>
-  )
+  );
 }
